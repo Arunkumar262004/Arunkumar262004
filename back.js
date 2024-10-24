a9b9b7a1621d84516e5723f73cda1859c7f4a26a
Arunkumar262004 <arunkumar957877@gmail.com>
2024-10-24 12:46:34 +0530

    Add files via upload

---
+++ back.js
@@ -0,0 +1,7 @@
+var sidenav = document.querySelector(".sidenavbar"); // querySelector to select a single element
+function shownavbar() {
+    sidenav.style.display = "block";
+}
+function Closenavbar() {
+    sidenav.style.display = "none"; // Corrected syntax
+}
